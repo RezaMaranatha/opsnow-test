386c730342ccbc14866af143fb7359a1d6bfdac3
Reza Maranatha <rezamaranatha@MacBook-Pro.local>
2025-10-26 14:40:34 +0700

    init FE and BE

---
+++ opsnow-be/src/main/java/com/ram/opsnow/OpsnowApplication.java
@@ -0,0 +1,13 @@
+package com.ram.opsnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpsnowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpsnowApplication.class, args);
+	}
+
+}
---
+++ opsnow-be/src/test/java/com/ram/opsnow/OpsnowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ram.opsnow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpsnowApplicationTests {
+
+	// @Test
+	// void contextLoads() {
+	// }
+
+}
